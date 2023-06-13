2730a9c31f48c1fa1b3fb68f658dc9dd9c4d68fc
Vibrill306 <117178465+Vibrill306@users.noreply.github.com>
2023-06-13 11:58:31 +0530

    Add files via upload

---
+++ assets/js/owl-carousel/jQueryCarousal.js
@@ -0,0 +1,112 @@
+$('.first-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause:false,
+    animateOut: 'fadeOut',
+    dots: false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.second-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause:false,
+    animateOut: 'fadeOut',
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+$('.third-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause:false,
+    animateOut: 'fadeOut',
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.fourth-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause:false,
+    animateOut: 'fadeOut',
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        700:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.fifth-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause:false,
+    animateOut: 'fadeOut',
+    dots: false,
+    responsive:{
+        0:{
+            items:2
+        },
+
+        600:{
+            items:3
+        }
+    }
+})
+
+
+
+
